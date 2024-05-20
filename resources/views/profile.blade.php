7a0125d5cb010a34dfaf832c2ad5542fd6f19c1d
Fathur Rizqi <fathurrizqizdjafar@gmail.com>
2024-05-20 23:11:53 +0800

    tailwind Dashboard

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,10 +2,13 @@
 
 
 @section('container')
-<ol>
-    @foreach ($data as $item)
-        <li>{{$item->name}}</li>
-    @endforeach
-</ol>
+
+    <ol>
+        @foreach ($data as $item)
+            <li> <span> {{ $item->id }}</span> {{$item->name}} <span>{{ $item->email }}</span></li>
+        @endforeach
+    </ol>
+
+
 @endsection
 
